6ad12ac0687d5a9d600d411d4ac5cb6c87336ddf
StefanWerW <scaw.dev@gmail.com>
2016-08-26 03:45:27 +0200

    fixes on google client env vars

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -4,8 +4,8 @@ import fs from 'fs';
 /** Repeat: DO! NOT! COMMIT! THIS! FILE! TO! YOUR! REPO! **/
 export const sessionSecret = process.env.SESSION_SECRET || 'Your Session Secret goes here';
 export const google = {
-  clientID: process.env.GOOGLE_CLIENTID || JSON.parse(fs.readFileSync('server/config/client_secret.json', 'utf8')).web.client_id,
-  clientSecret: process.env.GOOGLE_SECRET || JSON.parse(fs.readFileSync('server/config/client_secret.json', 'utf8')).web.client_secret,
+  clientID: process.env.GOOGLE_CLIENT_ID || JSON.parse(fs.readFileSync('server/config/client_secret.json', 'utf8')).web.client_id,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET || JSON.parse(fs.readFileSync('server/config/client_secret.json', 'utf8')).web.client_secret,
   callbackURL: process.env.GOOGLE_CALLBACK || '/auth/google/callback'
 };
 
